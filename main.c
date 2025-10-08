8e77c819639f5bf9a4a44bcdd121ca012883e8d1
10934248 <10934248@itisrossi.vi.it>
2025-10-08 11:29:50 +0200

    Polindromo

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    char palindormo[] = {"I topi non avevano nipoti"};
+    char *p_start = palindormo;
+    char *p_end = palindormo;
+    while (*p_end != '\0') {
+        p_end++;
+    }
+    p_end--;
+    int pal = 1;
+
+    while (p_start < p_end) {
+        if (*p_start != *p_end) {
+            pal = 0;
+            break;
+        }
+        p_start++;
+        p_end--;
+    }
+    if (pal) {
+        printf("palindromo\n");
+    } else {
+        printf("non palindromo\n");
+    }
+
+
     return 0;
 }
