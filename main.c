6bb4c84bdb3a4ba1de03d74798cf0cca6c954f8d
10934248 <10934248@itisrossi.vi.it>
2025-10-15 10:47:34 +0200

    Polindromo finitop

--- main.c
+++ main.c
@@ -4,26 +4,34 @@ int main(void) {
     char palindormo[] = {"I topi non avevano nipoti"};
     char *p_start = palindormo;
     char *p_end = palindormo;
+
     while (*p_end != '\0') {
         p_end++;
     }
     p_end--;
+
     int pal = 1;
 
     while (p_start < p_end) {
-        if (*p_start != *p_end) {
-            pal = 0;
-            break;
+
+        if (*p_start == ' ') {
+            p_start++;
+            continue;
+        }
+
+        if (*p_end == ' ') {
+            p_end--;
+            continue;
         }
         p_start++;
         p_end--;
     }
+
     if (pal) {
         printf("palindromo\n");
     } else {
         printf("non palindromo\n");
     }
-
 
     return 0;
 }
